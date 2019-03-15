31ed8b27c6ebe0a5e54ac3e8582aa1955e5a2e73
osmartin22 <oemartinez@ucdavis.edu>
2019-03-15 01:08:31 -0700

    Added target type to Move data

---
+++ src/main/java/ozmar/PokemonBattle/enums/PokeTarget.java
@@ -0,0 +1,25 @@
+package ozmar.PokemonBattle.enums;
+
+public enum PokeTarget {
+    ANY_ADJ_FOE_POKE(0),
+    ANY_ADJ_POKE(1),
+    ANY_POKE(2),
+    USER_OR_ANY_ADJ_ALLY(3),
+    ALL_ADJ_FOES(4),
+    ALL_ADJ_POKE(5),
+    ALL_FOES(6),
+    ALL_POKE(7),
+    ONE_ADJ_ALLY(8),
+    USER(9),
+    USER_AND_ALLIES(10);
+
+    private final int id;
+
+    PokeTarget(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
